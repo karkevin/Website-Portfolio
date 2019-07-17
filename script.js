991f895d015c81628b2007f4ae35098a1354efd2
Kevin Xu <karkevin@gmail.com>
2019-07-16 19:08:34 -0600

    Color Scrolling

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Slides the navigation into view for mobile devices. */
 const slideNavigation = function () {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
@@ -18,39 +19,64 @@ const slideNavigation = function () {
         });
     };
 
-};
+}
 
-function typeWriter(text, id, callback) {
+/* Typewriter effect for Hello World header. */
+function typeWriter(id, callback) {
     var i = 0;
     var speed = 95;
     var helloText = document.querySelector(id);
+    text = helloText.dataset.content;
+    helloText.innerHTML = "|";
 
+    /* Types the words across the screen, and calls the callback function when done.*/
     function typing(callback) {
         if (i < text.length) {
             helloText.innerHTML = helloText.innerHTML.slice(0, i) + text.charAt(i) + helloText.innerHTML.slice(i);
-            // helloText.textContent += text.charAt(i);
             i++;
             setTimeout(typing, speed, callback);
         }
         else {
             callback();
         }
-        
+
     }
     typing(callback);
 
 }
-typeWriter("Hello World! I'm Kevin Xu.", "#hello-text", blink);
 
+/* Blinks the "cursor" when text is finished writing. */
 function blink() {
     helloText = document.querySelector("#hello-text");
     helloText.innerHTML = helloText.innerHTML.slice(0, -1);
     var span = document.createElement("span");
     span.textContent = "|";
-    span.classList = "toggle-cursor";
+    span.classList.add("toggle-cursor");
     helloText.appendChild(span);
 }
 
+/* Activated when a navigation link is clicked. */
+function navClick() {
+    var aboutMe = document.querySelector("#about-me-link");
+    var resume = document.querySelector("#resume-link");
+    var projects = document.querySelector("#projects-link");
+    var logo = document.querySelector("#logo");
+    var duration = 1200;
+    aboutMe.addEventListener("click", function () {
+        smoothScroll("#about-me", duration);
+    });
+    resume.addEventListener("click", function () {
+        smoothScroll("#resume", duration);
+    });
+    projects.addEventListener("click", function () {
+        smoothScroll("#projects", duration);
+    });
+    logo.addEventListener("click", function () {
+        smoothScroll("#hello", duration);
+    });
+};
+
+/* Smoothly scrolls to different sections of the page. */
 const smoothScroll = function (target, duration) {
     var target = document.querySelector(target);
     var headerHeight = document.querySelector("header").offsetHeight - 2;
@@ -80,27 +106,36 @@ const smoothScroll = function (target, duration) {
     requestAnimationFrame(animate);
 }
 
-
-
-function activites() {
-    slideNavigation();
-    var aboutMe = document.querySelector("#about-me-link");
-    var resume = document.querySelector("#resume-link");
-    var projects = document.querySelector("#projects-link");
-    var logo = document.querySelector("#logo");
-    var duration = 1200;
-    aboutMe.addEventListener("click", function () {
-        smoothScroll("#about-me", duration);
-    });
-    resume.addEventListener("click", function () {
-        smoothScroll("#resume", duration);
-    });
-    projects.addEventListener("click", function () {
-        smoothScroll("#projects", duration);
-    });
-    logo.addEventListener("click", function () {
-        smoothScroll("#hello", duration);
+/* Background colour changes upon scrolling to different sections of the page. */
+function colourScroll(target) {
+    var sections = document.querySelectorAll("section");
+    var body = document.querySelector("body");
+    window.addEventListener("scroll", function () {
+        sections.forEach(element => {
+            var headerHeight = document.querySelector("header").offsetHeight - 2;
+            var positionTop = element.getBoundingClientRect().top - headerHeight;
+            var positionBottom = element.getBoundingClientRect().bottom;
+            var pixels = 300;
+            if (Math.abs(positionTop) < pixels || (positionTop < 0 && positionBottom > 0)) {
+                var colour = element.dataset.colour;
+                body.style.backgroundColor = colour;
+                this.console.log(positionTop + " " + positionBottom);
+            }
+        });
     });
 }
 
+/* Container function for all activities for the page. */
+function activites() {
+    window.onbeforeunload = function () {
+        var body = document.querySelector("body");
+        body.style.backgroundColor = "#c8f2ff";
+        window.scrollTo(0, 0);
+    };
+
+    typeWriter("#hello-text", blink);
+    colourScroll();
+    slideNavigation();
+    navClick();
+}
 activites();
